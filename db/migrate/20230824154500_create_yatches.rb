8bd69c19f726e25ab1b59d76ac1b71b7c2c148af
Lugard Agu <agulugard@gmail.com>
2023-08-24 16:56:56 +0100

    Generate models for user yatch and reservation

---
+++ db/migrate/20230824154500_create_yatches.rb
@@ -0,0 +1,13 @@
+class CreateYatches < ActiveRecord::Migration[7.0]
+  def change
+    create_table :yatches do |t|
+      t.string :model
+      t.string :picture
+      t.string :captain_name
+      t.decimal :price
+      t.references :user, null: false, foreign_key: true, on_delete: :cascade
+
+      t.timestamps
+    end
+  end
+end
