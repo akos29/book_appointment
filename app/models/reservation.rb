6317544a0021484973516bb90192ee5f77a48c3e
Mahabub Alam <mahabubx7@gmail.com>
2023-08-25 15:28:03 +0600

    implements(models): added-yatchs-user-reservation-models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,7 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :yatchs
+
+  validates :date, presence: true
+  validates :city, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :yatchs
+  has_many :reservations
+
+  validates :name, presence: true, length: { maximum: 50 }
+  # email will be added while work on devise
 end
