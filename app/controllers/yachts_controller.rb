00833479d9569636a35a0ae7169a2657fcf49c2f
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-28 15:11:23 +0200

    Add yachts controller

---
+++ app/controllers/yachts_controller.rb
@@ -0,0 +1,15 @@
+class YachtsController < ApplicationController
+  def index
+    @yachts = Yacht.all
+  end
+
+  def create 
+    @yacht = Yacht.new(yacht_params)
+    @yacht.user_id = current_user.id
+    if @yacht.save
+      redirect_to yacht_path(@yacht)
+    else
+      render :new
+    end
+  end
+end
--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -4,7 +4,6 @@ class Yacht < ApplicationRecord
   has_one_attached :yacht_image
 
   validates :model, presence: true
-  validates :picture, presence: true
   validates :captain_name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   
