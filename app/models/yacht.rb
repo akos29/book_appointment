c893900333beb3a853cf2b8991212a23eb1679cd
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-28 15:32:31 +0200

    add validation to Yacht

--- app/models/yacht.rb
+++ app/models/yacht.rb
@@ -6,5 +6,19 @@ class Yacht < ApplicationRecord
   validates :model, presence: true
   validates :captain_name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :yacht_image, presence: true
+
+  def acceptable_image
+    return unless yacht_image.attached?
+  
+    unless yacht_image.blob.byte_size <= 1.megabyte
+      errors.add(:yacht_image, "is too big")
+    end
+  
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(yacht_image.content_type)
+      errors.add(:yacht_image, "must be a JPEG or PNG")
+    end
+  end
   
 end
