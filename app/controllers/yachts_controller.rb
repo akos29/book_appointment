955a2597a35c464bb1c8574730c4da205d689d39
Mahabub Alam <mahabubx7@gmail.com>
2023-08-28 21:36:55 +0600

    yachts(display): yachts-controller-api-support-display

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -1,15 +1,28 @@
 class YachtsController < ApplicationController
+  include RackSessionsFix
+  respond_to :json
+
   def index
     @yachts = Yacht.all
+    render json: {
+      status: { code: 200, message: 'Display yachts.' },
+      data: @yachts
+    }
   end
 
   def create
     @yacht = Yacht.new(yacht_params)
+    puts yacht_params
     @yacht.user_id = current_user.id
     if @yacht.save
-      redirect_to yacht_path(@yacht)
+      render json: {
+        status: { code: 201, message: 'New yachts added.' },
+        data: @yacht
+      }
     else
-      render :new
+      render json: {
+        status: { message: 'Something went wrong!' }
+      }, status: :unprocessable_entity
     end
   end
 end
