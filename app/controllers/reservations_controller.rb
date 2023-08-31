4e7ad5153eb6b9acc79172b86e80e204f4d822cc
Kibrewossen Y Mekasha <kibrish49@gmail.com>
2023-08-31 23:00:23 +0200

    Update response status in reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,21 +1,30 @@
 class ReservationsController < ApplicationController
-  before_action :set_yacht
+  before_action :set_yacht, :set_user, only: [:create, :index]
 
   def index
     # @reservations = current_user.reservations.includes(:user, yacht: :user)
-    @user= User.first # after the login implementation we will change this to current_user
+
     @reservations = @user.reservations.includes(:user, yacht: :user)
     render json: @reservations.to_json(include: { user: {}, yacht: { include: :user } })
   end
 
   def create
+    puts params.inspect
     reservation = @yacht.reservations.build(reservation_params)
-    reservation.user = current_user
+    reservation.user = current_user || @user # after the login implementation we will change this to current_user
 
+    puts reservation.inspect
     if reservation.save
-      render json: { message: 'Reservation created successfully' }, status: :created
+      render json: {
+        success: true,
+        message: 'Reservation created successfully',
+        reservation: reservation.as_json  # Include the reservation details
+      }, status: :created
     else
-      render json: { error: reservation.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      render json: {
+        success: false,
+        error: reservation.errors.full_messages.join(', ')
+      }, status: :unprocessable_entity
     end
   end
 
@@ -25,7 +34,11 @@ class ReservationsController < ApplicationController
     @yacht = Yacht.find(params[:yacht_id])
   end
 
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
   def reservation_params
-    params.require(:reservation).permit(:date, :city)
+    params.require(:reservation).permit(:date, :city, :user_id, :yacht_id)
   end
 end
