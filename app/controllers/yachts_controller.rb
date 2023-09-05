df6f8993497dba53bd777b0107dc4716dd907848
Lugard Agu <agulugard@gmail.com>
2023-09-05 14:45:25 +0100

    Fix rubocop issue

--- app/controllers/yachts_controller.rb
+++ app/controllers/yachts_controller.rb
@@ -19,8 +19,7 @@ class YachtsController < ApplicationController
     # The after_create callback will be called after the yacht has been saved.
   end
 
-  def edit
-  end
+  def edit; end
 
   def show
     @yacht = Yacht.find(params[:id])
