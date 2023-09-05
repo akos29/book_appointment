8724432d917cf83139b271c0f08af8e9c05cfa12
Mahabub Alam <mahabubx7@gmail.com>
2023-09-05 15:39:16 +0600

    tdd(pre-config): refactor-devise-secret-credentials

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -313,7 +313,7 @@ Devise.setup do |config|
   #
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
